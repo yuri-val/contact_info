6dc758b220fee9d2d957c4ecc3d54a93b6a24deb
Yuri V <yuri.valigursky@gmail.com>
2018-07-02 15:37:52 +0300

    ff

---
+++ lib/generators/contact_info/install_generator.rb
@@ -0,0 +1,33 @@
+require 'rails/generators/base'
+
+module ContactInfo
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+
+      def self.next_migration_number(path)
+        ActiveRecord::Generators::Base.next_migration_number(path)
+      end
+
+      desc 'Installs and generates the necessary configuration files'
+      source_root File.expand_path('../templates', __FILE__)
+
+      def create_migrations
+        migration_template 'migrations/create_contact_info.rb',
+                           'db/migrate/create_contact_info.rb',
+                           migration_version: migration_version
+      end
+
+      def rails5?
+        Rails.version.start_with? '5'
+      end
+
+      def migration_version
+        if rails5?
+          "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
+        end
+      end
+    end
+  end
+end
+
