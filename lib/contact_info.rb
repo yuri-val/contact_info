1fd8d1e4cf7cc5876213b6a60ef2d5baf7b0a617
Yuri V <yuri.valigursky@gmail.com>
2018-07-02 16:02:02 +0300

    has_contact_info

--- lib/contact_info.rb
+++ lib/contact_info.rb
@@ -1,5 +1,11 @@
 require "contact_info/engine"
 
 module ContactInfo
-  # Your code goes here...
+  module ClassMethods
+    def has_contact_info(types = %w(address email phone))
+      types.each do |type|
+        define_method(type) { puts "You call method #{type}!" }
+      end
+    end
+  end
 end
